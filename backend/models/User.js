b7f5802aa7a9546c52db0c00c6cea4b6c27f3793
Sagar Arora <arorasagar540@gmail.com>
2026-03-14 18:51:06 +0530

    fix: resolve pre-save TypeError and missing mongoose ReferenceError in auth route

--- backend/models/User.js
+++ backend/models/User.js
@@ -60,14 +60,9 @@ const UserSchema = new mongoose.Schema({
 });
 
 // Hash password before saving
-UserSchema.pre('save', async function(next) {
-  if (!this.isModified('password')) return next();
-  try {
-    this.password = await bcrypt.hash(this.password, 10);
-    next();
-  } catch (err) {
-    next(err);
-  }
+UserSchema.pre('save', async function() {
+  if (!this.isModified('password')) return;
+  this.password = await bcrypt.hash(this.password, 10);
 });
 
 // Compare password
